56f607652372f4e27ad0f4dfa7bd268e4d6e647d
bayirdomuzu-dev <gefchj39@gmail.com>
2025-03-18 07:10:34 +0300

    update

---
+++ NeonSpor/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 4
+
+cloudstream {
+    authors     = listOf("bayirdomuzu-dev")
+    language    = "tr"
+    description = "(Tabii Spor Hariç Diğerleri için VPN Gerekebilir.) NeonSpor eklentisini de BeIN Spor, Tabii Spor ve S Spor kanalları mevcuttur."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=colorhunt.co&sz=%size%"
+}
