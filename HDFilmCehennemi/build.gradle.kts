9f8c070ea997a0fd900aad0fa99139667a1daaec
primatzeka <gefchj39@gmail.com>
2025-03-19 21:46:20 +0300

    update

---
+++ HDFilmCehennemi/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 4
+
+cloudstream {
+    authors     = listOf("primatzeka")
+    language    = "tr"
+    description = "(HDFilmCehennemi, en yeni filmleri ve yabancı dizileri HD kalitesinde izleyebileceğiniz ücretsiz bir platformdur."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi12.org&sz=%size%"
+}
