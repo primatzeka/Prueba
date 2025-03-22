68a13c8de58298a22bae152e460049a0c8fa6c8a
primatzeka <gefchj39@gmail.com>
2025-03-22 13:21:59 +0300

    update HDFilmCehennemi.kt

--- HDFilmCehennemi/src/main/kotlin/com/Prueba/HDFilmCehennemi.kt
+++ HDFilmCehennemi/src/main/kotlin/com/Prueba/HDFilmCehennemi.kt
@@ -8,7 +8,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 
 class HDFilmCehennemi : MainAPI() {
-    override var mainUrl              = "https://hdfilmcehennemi14.org"
+    override var mainUrl              = "https://hdfilmcehennemi9.org"
     override var name                 = "HDFilmCehennemi"
     override val hasMainPage          = true
     override var lang                 = "tr"
