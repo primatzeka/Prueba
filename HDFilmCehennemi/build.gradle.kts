9fd08ed2581511dafcc9c81b44dffba3eb4170cd
primatzeka <gefchj39@gmail.com>
2025-03-22 11:24:23 +0300

    update

--- HDFilmCehennemi/build.gradle.kts
+++ HDFilmCehennemi/build.gradle.kts
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("TvSeries")
-    iconUrl = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi12.org&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi13.org&sz=%size%"
 }
